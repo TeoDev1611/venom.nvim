5c201af8af0b395c4f1ee576835e9d712b0a7b78
TeoDev1611 <malehurtadoreyes@hotmail.com>
2021-11-04 17:58:20 -0500

    feat: Initialize the plugin with pipenv support

---
+++ python/venom.py
@@ -0,0 +1,49 @@
+import json
+import subprocess
+from sys import exit
+import logging
+from optparse import OptionParser
+import os
+
+
+def main():
+    # Create the parser from optparse
+    parser = OptionParser()
+
+    # Define the program options and description
+    parser.version = "1.0"
+    parser.description = (
+        "Venom utilitie for create a pipenv or poetry config for pyright"
+    )
+    parser.prog = "venom"
+
+    # Add the options for the parser
+
+    parser.add_option("-p", "--pipenv", action="store_true", dest="pipenv")
+    parser.add_option("-o", "--poetry", action="store_true", dest="poetry")
+
+    # Parse the args
+    (options, args) = parser.parse_args()
+
+    if options.pipenv:
+        pipenv_info()
+
+
+def pipenv_info():
+    file_exists = os.path.exists("./Pipfile")
+    if not file_exists:
+        logging.error("Pipfile not exists")
+        exit(2)
+    cmd = subprocess.run(["pipenv", "--venv"], stdout=subprocess.PIPE)
+    cmd_data = cmd.stdout.decode("utf-8")
+    data = {
+        "venvPath": cmd_data[:-3],
+    }
+    file = open("./pyrightconfig.json", "w")
+    json.dump(data, file)
+    file.close()
+    logging.info("File created successfully")
+
+
+if __name__ == "__main__":
+    main()
